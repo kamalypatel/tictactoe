9d48835f05a29bb217412b792830b7d7819effb1
Kamal <kamalpat@buffalo.edu>
2021-09-26 10:51:52 -0400

    Added winner statement and disable all feature

--- myscripts.js
+++ myscripts.js
@@ -9,12 +9,14 @@ btn1.addEventListener('click', () => {
         input.textContent = 'X'
         btn1.appendChild(input);
         document.getElementById("one").disabled = true;
+        checkWinnerOne();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn1.appendChild(input);
         document.getElementById("one").disabled = true;
+        checkWinnerOne();
         currentMove = playerOne;
     };})
 
@@ -25,12 +27,14 @@ btn2.addEventListener('click', () => {
         input.textContent = 'X'
         btn2.appendChild(input);
         document.getElementById("two").disabled = true;
+        checkWinnerTwo();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn2.appendChild(input);
         document.getElementById("two").disabled = true;
+        checkWinnerTwo();
         currentMove = playerOne;
     };})
 
@@ -41,12 +45,14 @@ btn3.addEventListener('click', () => {
         input.textContent = 'X'
         btn3.appendChild(input);
         document.getElementById("three").disabled = true;
+        checkWinnerThree();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn3.appendChild(input);
         document.getElementById("three").disabled = true;
+        checkWinnerThree();
         currentMove = playerOne;
     };})
 
@@ -57,12 +63,14 @@ btn4.addEventListener('click', () => {
         input.textContent = 'X'
         btn4.appendChild(input);
         document.getElementById("four").disabled = true;
+        checkWinnerFour();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn4.appendChild(input);
         document.getElementById("four").disabled = true;
+        checkWinnerFour();
         currentMove = playerOne;
     };})
 
@@ -73,12 +81,14 @@ btn5.addEventListener('click', () => {
         input.textContent = 'X'
         btn5.appendChild(input);
         document.getElementById("five").disabled = true;
+        checkWinnerFive();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn5.appendChild(input);
         document.getElementById("five").disabled = true;
+        checkWinnerFive();
         currentMove = playerOne;
     };})
 
@@ -89,12 +99,14 @@ btn6.addEventListener('click', () => {
         input.textContent = 'X'
         btn6.appendChild(input);
         document.getElementById("six").disabled = true;
+        checkWinnerSix();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn6.appendChild(input);
         document.getElementById("six").disabled = true;
+        checkWinnerSix();
         currentMove = playerOne;
     };})
 
@@ -105,12 +117,14 @@ btn7.addEventListener('click', () => {
         input.textContent = 'X'
         btn7.appendChild(input);
         document.getElementById("seven").disabled = true;
+        checkWinnerSeven();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn7.appendChild(input);
         document.getElementById("seven").disabled = true;
+        checkWinnerSeven();
         currentMove = playerOne;
     };})
 
@@ -121,12 +135,14 @@ btn8.addEventListener('click', () => {
         input.textContent = 'X'
         btn8.appendChild(input);
         document.getElementById("eight").disabled = true;
+        checkWinnerEight();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn8.appendChild(input);
         document.getElementById("eight").disabled = true;
+        checkWinnerEight();
         currentMove = playerOne;
     };})
 
@@ -137,11 +153,199 @@ btn9.addEventListener('click', () => {
         input.textContent = 'X'
         btn9.appendChild(input);
         document.getElementById("nine").disabled = true;
+        checkWinnerNine();
         currentMove = playerTwo;
     } else if (currentMove == playerTwo) {
         const input = document.createElement('p');
         input.textContent = 'O'
         btn9.appendChild(input);
         document.getElementById("nine").disabled = true;
+        checkWinnerNine();
         currentMove = playerOne;
     };})
+
+function checkWinnerOne() {
+    if (document.getElementById('one').textContent == document.getElementById('two').textContent && document.getElementById('one').textContent == document.getElementById('three').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('one').textContent == document.getElementById('four').textContent && document.getElementById('one').textContent == document.getElementById('seven').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('one').textContent == document.getElementById('five').textContent && document.getElementById('one').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerTwo() {
+    if (document.getElementById('one').textContent == document.getElementById('two').textContent && document.getElementById('one').textContent == document.getElementById('three').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('two').textContent == document.getElementById('five').textContent && document.getElementById('two').textContent == document.getElementById('eight').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerThree() {
+    if (document.getElementById('one').textContent == document.getElementById('two').textContent && document.getElementById('one').textContent == document.getElementById('three').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('three').textContent == document.getElementById('six').textContent && document.getElementById('three').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('three').textContent == document.getElementById('five').textContent && document.getElementById('three').textContent == document.getElementById('seven').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerFour() {
+    if (document.getElementById('four').textContent == document.getElementById('five').textContent && document.getElementById('four').textContent == document.getElementById('six').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('one').textContent == document.getElementById('four').textContent && document.getElementById('one').textContent == document.getElementById('seven').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerFive() {
+    if (document.getElementById('four').textContent == document.getElementById('five').textContent && document.getElementById('four').textContent == document.getElementById('six').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('two').textContent == document.getElementById('five').textContent && document.getElementById('two').textContent == document.getElementById('eight').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('one').textContent == document.getElementById('five').textContent && document.getElementById('one').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerSix() {
+    if (document.getElementById('four').textContent == document.getElementById('five').textContent && document.getElementById('four').textContent == document.getElementById('six').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('three').textContent == document.getElementById('six').textContent && document.getElementById('three').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerSeven() {
+    if (document.getElementById('one').textContent == document.getElementById('four').textContent && document.getElementById('one').textContent == document.getElementById('seven').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('seven').textContent == document.getElementById('eight').textContent && document.getElementById('seven').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('three').textContent == document.getElementById('five').textContent && document.getElementById('three').textContent == document.getElementById('seven').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerEight() {
+    if (document.getElementById('two').textContent == document.getElementById('five').textContent && document.getElementById('two').textContent == document.getElementById('eight').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('seven').textContent == document.getElementById('eight').textContent && document.getElementById('seven').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function checkWinnerNine() {
+    if (document.getElementById('seven').textContent == document.getElementById('eight').textContent && document.getElementById('seven').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('three').textContent == document.getElementById('six').textContent && document.getElementById('three').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    } else if (document.getElementById('one').textContent == document.getElementById('five').textContent && document.getElementById('one').textContent == document.getElementById('nine').textContent) {
+        const winner = document.querySelector('#winner');
+        const statement = document.createElement('p');
+        statement.textContent = `Congradulations ${currentMove}, you win the game!`;
+        winner.appendChild(statement);
+        disableAll();
+    };
+}
+
+function disableAll() {
+    document.getElementById("one").disabled = true;
+    document.getElementById("two").disabled = true;
+    document.getElementById("three").disabled = true;
+    document.getElementById("four").disabled = true;
+    document.getElementById("five").disabled = true;
+    document.getElementById("six").disabled = true;
+    document.getElementById("seven").disabled = true;
+    document.getElementById("eight").disabled = true;
+    document.getElementById("nine").disabled = true;
+}
